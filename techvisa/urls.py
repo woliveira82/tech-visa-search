b6d29fa73d5560fadca6d5c46d3cc182017565b4
woliveira82 <woliveira82@gmail.com>
2020-06-10 00:07:08 -0300

    Update urls

--- techvisa/urls.py
+++ techvisa/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('companies', views.post_company, name='post_company'),
 ]
--- techvisa/views.py
+++ techvisa/views.py
@@ -1,4 +1,6 @@
+from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Company
 
 
@@ -7,4 +9,14 @@ def index(request):
     data = {
         "company_list": company_list
     }
+    return render(request,'index.html', data)
+
+
+@require_http_methods(["POST"])
+def post_company(request):
+    company_list = request.FILES['file'].read()
+    data = csv.DictReader(company_list)
+    print(type(data))
+    print(data)
+
     return render(request,'index.html', data)
